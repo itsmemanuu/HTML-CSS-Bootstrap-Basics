74e7512b88a6b75a33966818619fc066765aff4e
Manuela Pacheco Malagón <m.pachecom2@uniandes.edu.co>
2023-04-23 17:50:44 -0500

    angular

---
+++ angular/src/app/serie/serie.ts
@@ -0,0 +1,5 @@
+export class Serie{
+  constructor(public id: number, public nombre: string, public network: string, public temporadas: number, public descripcion: string, public streaming: string, public imagen: string){
+
+  }
+}
